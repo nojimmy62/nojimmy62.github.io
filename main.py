8d197ebd6c1e6935f5b33816a6157562e84746a6
Hsiang-Chih Hwang <yesjimmy62@gmail.com>
2022-08-16 11:48:21 -0400

    test

---
+++ main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+import jinja2
+import os
+
+#load template library
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+class MainHandler(webapp2.RequestHandler): #take you to the home page
+    def get(self):
+        template = jinja_environment.get_template('index.html')
+        self.response.out.write(template.render())
+
+class MainHandler2(webapp2.RequestHandler):
+    def get(self, query):
+        print(query)
+    	if '.' not in query: #to remove .html in the url
+        	template = jinja_environment.get_template(query + '.html')
+        	self.response.out.write(template.render())
+        else:
+        	template = jinja_environment.get_template(query)
+        	self.response.out.write(template.render())
+
+
+
+class MainHandler3(webapp2.RequestHandler):
+    def get(self, query):
+        print(query)
+        template = jinja_environment.get_template(query)
+        self.response.out.write(template.render())
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+    (r'/(.*)', MainHandler2),
+], debug=True)
