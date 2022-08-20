8e2732cd42c269d3171871d59c4d8744879dd267
Hsiang-Chih Hwang <yesjimmy62@gmail.com>
2022-08-20 00:45:10 -0700

    events

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,17 +7,23 @@
     console.log(pageHeight)
     $(document).ready(function() {
         const imgElement = document.querySelector('#ImgContainer')
+        const eventsElement = document.querySelector('#EventsContainer')
 
         const ZOOM_STOPPOINT_1 = 1000
 
         const ZOOM_STOPPOINT_2 = 2000
+
+        const cover_zoom_frac = 0.6
+
         imgElement.style.top = 0.5 * (pageHeight - IMAGE_HEIGHT) + 'px'
+
+        eventsElement.style.top = ZOOM_STOPPOINT_1 + (pageHeight / 2 + cover_zoom_frac * IMAGE_HEIGHT / 2) + 'px'
 
         function anim() {
             let scroll = window.scrollY
             console.log("scroll:" + scroll)
             if (scroll < ZOOM_STOPPOINT_1) {
-                imgElement.style.transform = `scale(${0.5 + (1 - scroll / ZOOM_STOPPOINT_1)*0.5})`
+                imgElement.style.transform = `scale(${cover_zoom_frac + (1 - scroll / ZOOM_STOPPOINT_1)*(1-cover_zoom_frac)})`
                 imgElement.style.top = 0.5 * (pageHeight - IMAGE_HEIGHT) + 'px'
                 imgElement.style.position = 'fixed'
                 $("#text-marry").css("bottom", IMAGE_HEIGHT + "px")
@@ -26,7 +32,7 @@
 
                 // $("#text-marry").addClass("animation_fadein").css("position", "absolute").css("top", '-100px')
                 $("#text-marry").css("bottom", IMAGE_HEIGHT + "px")
-                imgElement.style.transform = `scale(${0.5})`
+                imgElement.style.transform = `scale(${cover_zoom_frac})`
                 imgElement.style.position = 'absolute'
                 imgElement.style.top = ZOOM_STOPPOINT_1 + (pageHeight / 2 - IMAGE_HEIGHT / 2) + 'px'
 
@@ -52,5 +58,7 @@
         }
 
         document.addEventListener('scroll', () => window.requestAnimationFrame(anim))
+
+        $(document).scrollTop(1450)
     });
 })(jQuery);
