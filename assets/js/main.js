b24cf0b37ffae5d795aa0c460415e54d31c9911c
PoHsien Wang <ddmail2009@gmail.com>
2022-08-20 10:08:25 -0700

    Remove unnecessary js code and some cleanup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,27 @@
-const ZOOM_STOPPOINT_1 = 1000
-const cover_zoom_frac = 0.6
+
+
 var scrollShrink = function() {
-    imgHeight = $("img").height()
-    pageHeight = $(window).height();
-    let scroll = window.scrollY
-    console.log("scroll:" + scroll)
-    $("#TextMarry").css("bottom", imgHeight + "px")
+    var ZOOM_STOPPOINT_1 = 1000
+    var imgHeight = $("#JabuMarryImg").height()
+    var pageHeight = $(window).height();
+    var maxScaleSize = 0.9
+    var minScaleSize = 0.6
+
+    $("#ScrollBlock").css('height', ZOOM_STOPPOINT_1 + pageHeight + "px")
+    var scroll = window.scrollY
     if (scroll < ZOOM_STOPPOINT_1) {
-        scale = cover_zoom_frac + (1 - scroll / ZOOM_STOPPOINT_1) * (1 - cover_zoom_frac) * 0.9
+        scale = minScaleSize + (1 - scroll / ZOOM_STOPPOINT_1) * (maxScaleSize-minScaleSize)
         $('#ImgContainer').css('transform', `scale(${scale})`)
         $('#ImgContainer').css('top', 0.5 * (pageHeight - imgHeight) + 'px')
         $('#ImgContainer').css('position', 'fixed')
     } else {
-        scale = cover_zoom_frac
+        scale = minScaleSize
         $('#ImgContainer').css('transform', `scale(${scale})`)
         $('#ImgContainer').css('top', ZOOM_STOPPOINT_1 + (pageHeight / 2 - imgHeight / 2) + 'px')
         $('#ImgContainer').css('position', 'absolute')
     }
 }
-var initialize = function() {
-    imgHeight = $("img").height()
-    pageHeight = $(window).height();
-    $('#EventsContainer').css('top', ZOOM_STOPPOINT_1 + (pageHeight / 2 + cover_zoom_frac * imgHeight / 2) + 'px')
-    $("#ScrollBlock").css('height', 1000 + document.documentElement.clientHeight + "px")
+var initialize = function() {    
     scrollShrink()
     window.addEventListener("resize", () => window.requestAnimationFrame(scrollShrink));
     document.addEventListener('scroll', () => window.requestAnimationFrame(scrollShrink))
