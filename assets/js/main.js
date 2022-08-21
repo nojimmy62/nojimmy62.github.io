1e2a9c9a07434e7e13323b664d17aafc85da0a13
PoHsien Wang <ddmail2009@gmail.com>
2022-08-20 23:10:52 -0700

    Use dynamic scroll height instead of fixed 1000px

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,23 +17,19 @@ var _updateConfig = []
 
 var animateUpdate = function() {
     var scroll = window.scrollY
-    
 
     if (scroll >= $("#WeddingEvent").offset().top && scroll < $("#OurStory1").offset().top) {
         $('#EventButton').addClass('active')
         $('#StoryButton').removeClass('active')
     }
-    if (scroll >= $("#OurStory1").offset().top-1) {
+    if (scroll >= $("#OurStory1").offset().top - 1) {
         $('#EventButton').removeClass('active')
         $('#StoryButton').addClass('active')
     }
 
-
     _updateConfig.forEach((config) => {
         var containerTop = $(config.selector).offset().top
         var element = $(config.selector).find('.shrinkElement')
-        //var pageHeight = $(window).height()
-        var pageHeight = element.height()
         if (scroll < containerTop) {
             scale = config.initialScale
             element.css('position', 'absolute')
@@ -45,7 +41,7 @@ var animateUpdate = function() {
         } else {
             scale = config.finalScale
             element.css('position', 'absolute')
-            element.css('top',containerTop + config.height + $(".sticky-top").height())
+            element.css('top', containerTop + config.height + $(".sticky-top").height())
         }
         element.css('transform', `scale(${scale})`)
     })
@@ -64,18 +60,19 @@ var ScrollShrink = function(config) {
     if (invisible.length == 0) {
         alert("No invisible-block found under " + config.selector)
     }
+    config.height = element.height()
     invisible.css('height', config.height + element.height())
     _updateConfig.push(config)
     animateUpdate()
 }
 
 var initialize = function() {
-    ScrollShrink(new ScrollConfig("#Cover", 0.9, 0.6, 1000))
-    ScrollShrink(new ScrollConfig("#WeddingEvent", 1, 0.6, 1000))
-    ScrollShrink(new ScrollConfig("#OurStory1", 1, 0.6, 1000))
-    ScrollShrink(new ScrollConfig("#OurStory2", 1, 0.6, 1000))
-    ScrollShrink(new ScrollConfig("#OurStory3", 1, 0.6, 1000))
-    ScrollShrink(new ScrollConfig("#OurStory4", 1, 0.6, 1000))
+    ScrollShrink(new ScrollConfig("#Cover", 0.9, 0.6))
+    ScrollShrink(new ScrollConfig("#WeddingEvent", 1, 0.6))
+    ScrollShrink(new ScrollConfig("#OurStory1", 1, 0.6))
+    ScrollShrink(new ScrollConfig("#OurStory2", 1, 0.6))
+    ScrollShrink(new ScrollConfig("#OurStory3", 1, 0.6))
+    ScrollShrink(new ScrollConfig("#OurStory4", 1, 0.6))
     $(window).on('resize', () => window.requestAnimationFrame(animateUpdate))
     $(document).on('scroll', () => window.requestAnimationFrame(animateUpdate))
     if ('scrollRestoration' in history) {
