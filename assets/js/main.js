802d8f2d5fe9dc04efa67e5a2097e194dccbe0df
Hsiang-Chih Hwang <yesjimmy62@gmail.com>
2022-08-21 01:58:33 -0700

    Fun facts added

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,6 +69,38 @@ langText = {
         "en": "Ting-Wei and Hsiang-Chih met each other in Baltimore, 2016. Well, they didn’t get close to each other right away, until they started to play badminton together! (Oh btw, Hsiang-Chih was a captain of the badminton team in his undergrad department) <br/> Then, they found that they were not only good teammates on the badminton court but also in life! Therefore, they became a couple in summer, 2017! ",
         "tw": "2016年，亭維和翔致在美國巴爾的摩求學時相遇。但大概是在半年後，他們才因一起打羽球才變熟(翔致表示，還好以前在大學系上羽球隊沒白費)。 <br/>日後，他們發現，他們不僅是羽球場上的好隊友，他們還可以一起玩音樂、划船、數星星。最後，在2017年的夏天，他們成了情侶!"
     },
+    "#TextStory5":{
+        "en": "It has been six years since Ting-Wei and Hsiang-Chih were together. Their daily life is full of funny moments. During the Covid, they witnessed Hsiang-Chih’s super long beard, as well as the fantastic comet in 2020. Hsiang-Chih often wolfed down the snacks, but not allowing Ting-Wei to do the same thing. Hsiang-Chih would also tell Ting-Wei not to sit with crossed legs, saying that it’s not good for her knees. <br/>Anyway, they seem to enjoy a lot their daily life! ",
+        "tw": "不知不覺，從在一起到現在，亭維和翔致邁入了第6年。生活上也是滿滿的點滴。在covid 期間，他們一起見證了翔致超級長的鬍子，跟難得一見的彗星。或者翔致常常吃零食都很大口，但又叫亭維不能學他。或者翔致會囉唆的叫亭維不能盤腿、對膝蓋不好...<br/>總之，他們似乎生活上的點點滴滴也是充滿著樂趣！"
+    },
+    "#TextStory6":{
+        "en": "After Hsiang-Chih finished his PhD in astronomy in 2021, Ting-Wei and Hsiang-Chih had a small celebration trip to Alaska, where their arms were completely exhausted after 5-hour kayaking. Then they moved to Princeton for their new jobs, and they thought it is time for next chapter of their life. <br/> Hsiang-Chih proposed to Ting-Wei on Feb. 12, 2022, promising that they will face the joy and difficulty together, and will get old together with laughter. And, Ting-Wei said yes :) ",
+        "tw": ""
+    },
+    "#TextStory7":{
+        "en": "Ting-Wei and Hsiang-Chih will have their wedding on Dec. 18, 2022. Please come and celebrate the big moment of their lives! Your presence means a lot to us!! ",
+        "tw": ""
+    },
+    ".factHeader":{
+        "en": "Fun facts",
+        "tw": "小趣事"
+    },
+    "#TextFact1":{
+        "en": "Do these strange animals have names? <br/> <br/> Yes! Their names are Jabu and Duki! The actual meanings of these names remain unclear. Jabu is often mistaken as an octopus (or its kind), but Jabu is definitely not an octopus. ",
+        "tw": ""
+    },
+    "#TextFact2":{
+        "en": "When is the “birthday” of Jabu? <br/> <br/>It is currently unknown. Based on Archeology, the earliest Jabu-related product was dated back to ~2005, when Hsiang-Chih was in his middle school. Since this Jabu product is already very mature, Jabu likely has been existent a few years before 2005. <br/>Btw, Hsiang-Chih also uses Jabu as his signature in his credit card payment. ",
+        "tw": ""
+    },
+    "#TextFact3":{
+        "en": "When is the “birthday” of Duki? <br/> <br/>The concept of Duki was first created in 2017, by Hsiang-Chih. But the drawing of Duki has been significantly improved (?) by Ting-Wei. In fact, Hsiang-Chih is not as good at drawing Duki due to its complexity.  ",
+        "tw": ""
+    },
+    "#TextFact4":{
+        "en": "How is this website being made? <br/><br/>This website is made by Ting-Wei (all the drawing), Hsiang-Chih (content), and Po-Hsien (tremendous technical help)! ",
+        "tw": ""
+    },
 }
 /*
 "":{
@@ -99,10 +131,17 @@ var animateUpdate = function() {
     if (scroll >= $("#WeddingEvent").offset().top && scroll < $("#OurStory1").offset().top) {
         $('#EventButton').addClass('active')
         $('#StoryButton').removeClass('active')
+        $('#FactButton').removeClass('active')
     }
-    if (scroll >= $("#OurStory1").offset().top - 1) {
+    if (scroll >= $("#OurStory1").offset().top - 1 && scroll < $("#Facts1").offset().top) {
         $('#EventButton').removeClass('active')
         $('#StoryButton').addClass('active')
+        $('#FactButton').removeClass('active')
+    }
+    if (scroll >= $("#Facts1").offset().top - 1) {
+        $('#EventButton').removeClass('active')
+        $('#StoryButton').removeClass('active')
+        $('#FactButton').addClass('active')
     }
 
     _updateConfig.forEach((config) => {
@@ -162,6 +201,13 @@ var initialize = function() {
     ScrollShrink(new ScrollConfig("#OurStory2", 1, 0.6))
     ScrollShrink(new ScrollConfig("#OurStory3", 1, 0.6))
     ScrollShrink(new ScrollConfig("#OurStory4", 1, 0.6))
+    ScrollShrink(new ScrollConfig("#OurStory5", 1, 0.6))
+    ScrollShrink(new ScrollConfig("#OurStory6", 1, 0.6))
+    ScrollShrink(new ScrollConfig("#OurStory7", 1, 0.6))
+    ScrollShrink(new ScrollConfig("#Facts1", 1, 0.6))
+    ScrollShrink(new ScrollConfig("#Facts2", 1, 0.6))
+    ScrollShrink(new ScrollConfig("#Facts3", 1, 0.6))
+    ScrollShrink(new ScrollConfig("#Facts4", 1, 0.6))
     $("#englishButton").click(function(){
         langChange("en");
         $(this).children('a').addClass("active");
