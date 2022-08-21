06644645e9544654ac3a0cee88ca79dc03e85150
Hsiang-Chih Hwang <yesjimmy62@gmail.com>
2022-08-21 16:28:45 -0700

    Fact Chinese

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,112 +1,113 @@
 langText = {
-        "#EventButton": {
-            "en": "Wedding events",
-            "tw": "婚禮資訊"
-        },
-        "#StoryButton": {
-            "en": "Our story",
-            "tw": "我們的故事"
-        },
-        "#FactButton": {
-            "en": "Fun facts",
-            "tw": "小趣事"
-        },
-        "#TextMarry": {
-            "en": "Ting-Wei & Hsiang-Chih are getting married!",
-            "tw": "亭維 & 翔致要結婚了！"
-        },
-        "#TextWedding": {
-            "en": "Wedding events",
-            "tw": "婚禮資訊"
-        },
-        "#TextDateHeader": {
-            "en": "Date",
-            "tw": "日期"
-        },
-        "#TextDate": {
-            "en": "December 18, 2022 (Sunday)",
-            "tw": "2022年12月18日 (星期日)"
-        },
-        "#TextCeremonyHeader": {
-            "en": "Ceremony",
-            "tw": "結婚典禮"
-        },
-        "#TextChurch": {
-            "en": "Chi-Nan Presbyterian Church, 3pm<br/> No. 3, Zhongshan S Rd, Zhongzheng District, Taipei City",
-            "tw": "濟南教會, 3:00 pm入場，3:30 典禮開始 <br/> \
-                   台北市中正區中山南路3號 <br/> \
-                   近捷運台大醫院站、善導寺站、台北車站，地圖連結"
-        },
-        "#TextBanquetHeader": {
-            "en": "Banquet",
-            "tw": "婚禮晚宴"
-        },
-        "#TextBanquet": {
-            "en": "Mandarin Oriental Taipei, 7pm<br/> \
-                   No. 158, DunHua N Rd, Songshan District, Taipei City",
-            "tw": "臺北文華東方酒店，6 pm入場，6:30 pm 開始<br/> \
-                   台北市松山區敦化北路158號<br/> \
-                   近捷運台北小巨蛋站"
-        },
-        ".storyHeader": {
-            "en": "Our story",
-            "tw": "我們的故事"
-        },
-        "#TextStory1": {
-            "en": "Once upon the time, Ting-Wei and Hsiang-Chih, both Taiwanese. They both grew up in Taipei, but never met each other.",
-            "tw": "很久很久以前，亭維和翔致都是台北人，但他們並沒有在台灣相遇。"
-        },
-        "#TextStory2": {
-            "en": "Hsiang-Chih, a nature lover, loves to travel around the world, and also stargazing. One of his most proudest things is he went to Tibet in 2014, counting stars with Mt. Everest. Maybe that’s how he ended up with his astronomy career later?",
-            "tw": "翔致熱愛自然，喜歡旅遊及觀星。他曾經去過西藏的喜馬拉雅山基地營看星星。這或許是為什麼後來翔致走上天文研究的原因？"
-        },
-        "#TextStory3": {
-            "en": "Ting-Wei is an enthusiastic music lover. She sings with playing piano or guitar. She writes songs on her own. Want to listen to one? Here you go: youtube link",
-            "tw": "亭維熱愛音樂。她能自彈(鋼琴和吉他)自唱，甚至還會自己寫歌。想聽聽她的作品嗎？這裡是連結！"
-        },
-        "#TextStory4": {
-            "en": "Ting-Wei and Hsiang-Chih met each other in Baltimore, 2016. Well, they didn’t get close to each other right away, until they started to play badminton together! (Oh btw, Hsiang-Chih was a captain of the badminton team in his undergrad department) <br/> Then, they found that they were not only good teammates on the badminton court but also in life! Therefore, they became a couple in summer, 2017! ",
-            "tw": "2016年，亭維和翔致在美國巴爾的摩求學時相遇。但大概是在半年後，他們才因一起打羽球才變熟(翔致表示，還好以前在大學系上羽球隊沒白費)。 <br/>日後，他們發現，他們不僅是羽球場上的好隊友，他們還可以一起玩音樂、划船、數星星。最後，在2017年的夏天，他們成了情侶!"
-        },
-        "#TextStory5": {
-            "en": "It has been six years since Ting-Wei and Hsiang-Chih were together. Their daily life is full of funny moments. During the Covid, they witnessed Hsiang-Chih’s super long beard, as well as the fantastic comet in 2020. Hsiang-Chih often wolfed down the snacks, but not allowing Ting-Wei to do the same thing. Hsiang-Chih would also tell Ting-Wei not to sit with crossed legs, saying that it’s not good for her knees. <br/>Anyway, they seem to enjoy a lot their daily life! ",
-            "tw": "不知不覺，從在一起到現在，亭維和翔致邁入了第6年。生活上也是滿滿的點滴。在covid 期間，他們一起見證了翔致超級長的鬍子，跟難得一見的彗星。或者翔致常常吃零食都很大口，但又叫亭維不能學他。或者翔致會囉唆的叫亭維不能盤腿、對膝蓋不好...<br/>總之，他們似乎生活上的點點滴滴也是充滿著樂趣！"
-        },
-        "#TextStory6": {
-            "en": "After Hsiang-Chih finished his PhD in astronomy in 2021, Ting-Wei and Hsiang-Chih had a small celebration trip to Alaska, where their arms were completely exhausted after 5-hour kayaking. Then they moved to Princeton for their new jobs, and they thought it is time for next chapter of their life. <br/> Hsiang-Chih proposed to Ting-Wei on Feb. 12, 2022, promising that they will face the joy and difficulty together, and will get old together with laughter. And, Ting-Wei said yes :) ",
-            "tw": ""
-        },
-        "#TextStory7": {
-            "en": "Ting-Wei and Hsiang-Chih will have their wedding on Dec. 18, 2022. Please come and celebrate the big moment of their lives! Your presence means a lot to us!! ",
-            "tw": ""
-        },
-        ".factHeader": {
-            "en": "Fun facts",
-            "tw": "小趣事"
-        },
-        "#TextFact1": {
-            "en": "Do these strange animals have names? <br/> <br/> Yes! Their names are Jabu and Duki! The actual meanings of these names remain unclear. Jabu is often mistaken as an octopus (or its kind), but Jabu is definitely not an octopus. ",
-            "tw": ""
-        },
-        "#TextFact2": {
-            "en": "When is the “birthday” of Jabu? <br/> <br/>It is currently unknown. Based on Archeology, the earliest Jabu-related product was dated back to ~2005, when Hsiang-Chih was in his middle school. Since this Jabu product is already very mature, Jabu likely has been existent a few years before 2005. <br/>Btw, Hsiang-Chih also uses Jabu as his signature in his credit card payment. ",
-            "tw": ""
-        },
-        "#TextFact3": {
-            "en": "When is the “birthday” of Duki? <br/> <br/>The concept of Duki was first created in 2017, by Hsiang-Chih. But the drawing of Duki has been significantly improved (?) by Ting-Wei. In fact, Hsiang-Chih is not as good at drawing Duki due to its complexity.  ",
-            "tw": ""
-        },
-        "#TextFact4": {
-            "en": "How is this website being made? <br/><br/>This website is made by Ting-Wei (all the drawing), Hsiang-Chih (content), and Po-Hsien (tremendous technical help)! ",
-            "tw": ""
-        },
-    }
-    /*
-    "":{
-            "en": "",
-            "tw": ""
-        },
-    */
+    "#EventButton": {
+        "en": "Wedding events",
+        "tw": "婚禮資訊"
+    },
+    "#StoryButton": {
+        "en": "Our story",
+        "tw": "我們的故事"
+    },
+    "#FactButton": {
+        "en": "Fun facts",
+        "tw": "小趣事"
+    },
+    "#TextMarry": {
+        "en": "Ting-Wei & Hsiang-Chih are getting married!",
+        "tw": "亭維 & 翔致要結婚了！"
+    },
+    "#TextWedding": {
+        "en": "Wedding events",
+        "tw": "婚禮資訊"
+    },
+    "#TextDateHeader": {
+        "en": "Date",
+        "tw": "日期"
+    },
+    "#TextDate": {
+        "en": "December 18, 2022 (Sunday)",
+        "tw": "2022年12月18日 (星期日)"
+    },
+    "#TextCeremonyHeader": {
+        "en": "Ceremony",
+        "tw": "結婚典禮"
+    },
+    "#TextChurch": {
+        "en": "Chi-Nan Presbyterian Church, 3pm<br/> No. 3, Zhongshan S Rd, Zhongzheng District, Taipei City",
+        "tw": "濟南教會, 3:00 pm入場，3:30 典禮開始 <br/> \
+               台北市中正區中山南路3號 <br/> \
+               近捷運台大醫院站、善導寺站、台北車站，地圖連結"
+    },
+    "#TextBanquetHeader": {
+        "en": "Banquet",
+        "tw": "婚禮晚宴"
+    },
+    "#TextBanquet": {
+        "en": "Mandarin Oriental Taipei, 7pm<br/> \
+               No. 158, DunHua N Rd, Songshan District, Taipei City",
+        "tw": "臺北文華東方酒店，6 pm入場，6:30 pm 開始<br/> \
+               台北市松山區敦化北路158號<br/> \
+               近捷運台北小巨蛋站"
+    },
+    ".storyHeader": {
+        "en": "Our story",
+        "tw": "我們的故事"
+    },
+    "#TextStory1": {
+        "en": "Once upon the time, Ting-Wei and Hsiang-Chih, both Taiwanese. They both grew up in Taipei, but never met each other.",
+        "tw": "很久很久以前，亭維和翔致都是台北人，但他們並沒有在台灣相遇。"
+    },
+    "#TextStory2": {
+        "en": "Hsiang-Chih, a nature lover, loves to travel around the world, and also stargazing. One of his most proudest things is he went to Tibet in 2014, counting stars with Mt. Everest. Maybe that’s how he ended up with his astronomy career later?",
+        "tw": "翔致熱愛自然，喜歡旅遊及觀星。他曾經去過西藏的喜馬拉雅山基地營看星星。這或許是為什麼後來翔致走上天文研究的原因？"
+    },
+    "#TextStory3": {
+        "en": "Ting-Wei is an enthusiastic music lover. She sings with playing piano or guitar. She writes songs on her own. Want to listen to one? Here you go: youtube link",
+        "tw": "亭維熱愛音樂。她能自彈(鋼琴和吉他)自唱，甚至還會自己寫歌。想聽聽她的作品嗎？這裡是連結！"
+    },
+    "#TextStory4": {
+        "en": "Ting-Wei and Hsiang-Chih met each other in Baltimore, 2016. Well, they didn’t get close to each other right away, until they started to play badminton together! (Oh btw, Hsiang-Chih was a captain of the badminton team in his undergrad department) <br/> Then, they found that they were not only good teammates on the badminton court but also in life! Therefore, they became a couple in summer, 2017! ",
+        "tw": "2016年，亭維和翔致在美國巴爾的摩求學時相遇。但大概是在半年後，他們才因一起打羽球才變熟(翔致表示，還好以前在大學系上羽球隊沒白費)。 <br/>日後，他們發現，他們不僅是羽球場上的好隊友，他們還可以一起玩音樂、划船、數星星。最後，在2017年的夏天，他們成了情侶!"
+    },
+    "#TextStory5": {
+        "en": "It has been six years since Ting-Wei and Hsiang-Chih were together. Their daily life is full of funny moments. During the Covid, they witnessed Hsiang-Chih’s super long beard, as well as the fantastic comet in 2020. Hsiang-Chih often wolfed down the snacks, but not allowing Ting-Wei to do the same thing. Hsiang-Chih would also tell Ting-Wei not to sit with crossed legs, saying that it’s not good for her knees. <br/>Anyway, they seem to enjoy a lot their daily life! ",
+        "tw": "不知不覺，從在一起到現在，亭維和翔致邁入了第6年。生活上也是滿滿的點滴。在covid 期間，他們一起見證了翔致超級長的鬍子，跟難得一見的彗星。或者翔致常常吃零食都很大口，但又叫亭維不能學他。或者翔致會囉唆的叫亭維不能盤腿、對膝蓋不好...<br/>總之，他們似乎生活上的點點滴滴也是充滿著樂趣！"
+    },
+    "#TextStory6": {
+        "en": "After Hsiang-Chih finished his PhD in astronomy in 2021, Ting-Wei and Hsiang-Chih had a small celebration trip to Alaska, where their arms were completely exhausted after 5-hour kayaking. Then they moved to Princeton for their new jobs, and they thought it is time for next chapter of their life. <br/> Hsiang-Chih proposed to Ting-Wei on Feb. 12, 2022, promising that they will face the joy and difficulty together, and will get old together with laughter. And, Ting-Wei said yes :) ",
+        "tw": ""
+    },
+    "#TextStory7": {
+        "en": "Ting-Wei and Hsiang-Chih will have their wedding on Dec. 18, 2022. Please come and celebrate the big moment of their lives! Your presence means a lot to us!! ",
+        "tw": ""
+    },
+    ".factHeader": {
+        "en": "Fun facts",
+        "tw": "小趣事"
+    },
+    "#TextFact1": {
+        "en": "Do these strange animals have names? <br/> <br/> Yes! Their names are Jabu and Duki! The actual meanings of these names remain unclear. Jabu is often mistaken as an octopus (or its kind), but Jabu is definitely not an octopus. ",
+        "tw": "這兩個生物有名字嗎？<br\><br\>有！他們的名字是Jabu和Duki。名字的意思不明。順帶一提，Jabu並不是章魚(也不是類似章魚的生物)。"
+    },
+    "#TextFact2": {
+        "en": "When is the “birthday” of Jabu? <br/> <br/>It is currently unknown. Based on Archeology, the earliest Jabu-related product was dated back to ~2005, when Hsiang-Chih was in his middle school. Since this Jabu product is already very mature, Jabu likely has been existent a few years before 2005. <br/>Btw, Hsiang-Chih also uses Jabu as his signature in his credit card payment. ",
+        "tw": "Jabu 是什麼時候誕生的？<br\><br\>Jabu的誕生日已不可考。但根據考古資料，目前最早的Jabu相關產品(下圖)可追溯至~2005年、翔致國中的美術課作業。但這個成品已展現 Jabu 作畫的高度成熟性，因此推估Jabu應於數年前就已誕生。<br\>順帶一提，翔致現在的信用卡簽名也是用Jabu。"
+    },
+    "#TextFact3": {
+        "en": "When is the “birthday” of Duki? <br/> <br/>The concept of Duki was first created in 2017, by Hsiang-Chih. But the drawing of Duki has been significantly improved (?) by Ting-Wei. In fact, Hsiang-Chih is not as good at drawing Duki due to its complexity.  ",
+        "tw": "Duki 是什麼時候誕生的？<br\><br\>第一個Duki是翔致於 2017年時畫的(下方左圖)。但現今的Duki已受到亭維大量的改良。其實，因為Duki較複雜的結構，翔致不太擅長畫Duki。"
+    },
+    "#TextFact4": {
+        "en": "How is this website being made? <br/><br/>This website is made by Po-Hsien (tremendous technical help), Ting-Wei (all the drawing), and Hsiang-Chih (content)!",
+        "tw": "這網站是如何製作的呢？<br\>這網站是由柏軒(大量的技術支援)、亭維(所有作圖)、翔致(內容)完成！"
+    },
+}
+/*
+"":{
+        "en": "",
+        "tw": ""
+    },
+*/
+
 function makeStruct(names) {
     var names = names.split(" ");
     var count = names.length;
